0d0e2e854d3a02b0aa9ff09bcc37b8a848d2166f
omrabari <25bpe065@spt.pdpu.ac.in>
2026-01-16 10:36:31 +0530

    Add files via upload

---
+++ netsalary.net.py
@@ -0,0 +1,7 @@
+gross=float(input("enter gross salary"))
+
+allow=0.10*gross
+ded=0.03*gross
+
+net=gross+allow-ded
+print("your net salary", net)
---
+++ netsales.py
@@ -0,0 +1,6 @@
+gross=float(input("enter gross sales"))
+
+discount=0.10*gross
+
+net=gross-discount
+print("your net sales", net)
