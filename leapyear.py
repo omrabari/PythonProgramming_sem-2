cdeb4fbc2b4a07169a151888b25d309d89c4717a
omrabari <25bpe065@spt.pdpu.ac.in>
2026-01-23 10:26:22 +0530

    Add files via upload

---
+++ leapyear.py
@@ -0,0 +1,16 @@
+a=int(input("enter ur year"))
+
+if a%4==0 and a%100!=0:
+
+
+    print("year is leap year")
+
+elif a%400==0:
+
+
+    print("year is leap year")
+
+else:
+
+
+    print("year is non leap year")
