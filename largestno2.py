da71e441ce07e7b32d508e0ff87887e1bb29bdfb
omrabari <25bpe065@spt.pdpu.ac.in>
2026-01-23 09:59:14 +0530

    Add files via upload

---
+++ largestno2.py
@@ -0,0 +1,19 @@
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+c=int(input("enter third number"))
+
+if a>b and a>c:
+    
+
+    print("the largest no is a=",a)
+
+elif a<b and b>c:
+
+
+    print("the largest no is b=",b)
+
+else:
+
+
+    print("the largest no is c=",c)
+
